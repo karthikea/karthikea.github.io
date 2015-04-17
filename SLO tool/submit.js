1b2e3be61dd5db015d6f74920de551b58d725b91
karthikea <kkrishnamurthy@EA4.EDANALYTICS.local>
2015-04-17 16:46:50 -0500

    submit now goes to the next tab

--- SLO tool/submit.js
+++ SLO tool/submit.js
@@ -38,6 +38,14 @@ function getMethod(){
     var val=$('#method').combobox('getText');
 	document.getElementById("selMet").innerHTML=val;
 }
+function getAssess1(){
+    var x=document.getElementById("assess1").value;
+	document.getElementById("a1").innerHTML=x;
+}
+function getAssess2(){
+    var x=document.getElementById("assess2").value;
+	document.getElementById("a2").innerHTML=x;
+}
 function submitElement0(){
     getYear();
 	getName();
@@ -45,22 +53,69 @@ function submitElement0(){
 	getCourse();
 	getGrade();
 	getInterval();
+	
+	
+ 
+        // Change tab 
+        jQuery("#tab1").attr('class','tab');
+		jQuery("#tab2").attr('class','tab active');
+		
+		jQuery("#t2").addClass('active');
+		jQuery("#t1").removeClass('active');
+ 
+      
 }
 function submitElement1(){
 	getGoal();
 	getStandards();
 	getPop();
 	getMethod();
+	
+	// Change tab 
+        jQuery("#tab2").attr('class','tab');
+		jQuery("#tab3").attr('class','tab active');
+		
+		jQuery("#t3").addClass('active');
+		jQuery("#t2").removeClass('active');
+	
 }
 function submitElement2(){
-
+    getAssess1();
+	getAssess2();
+	
+	// Change tab 
+        jQuery("#tab3").attr('class','tab');
+		jQuery("#tab4").attr('class','tab active');
+		
+		jQuery("#t4").addClass('active');
+		jQuery("#t3").removeClass('active');
 }
 function submitElement3(){
+
+	// Change tab 
+        jQuery("#tab4").attr('class','tab');
+		jQuery("#tab5").attr('class','tab active');
+		
+		jQuery("#t5").addClass('active');
+		jQuery("#t4").removeClass('active');
 
 }
 function submitElement4(){
 
+	// Change tab 
+        jQuery("#tab5").attr('class','tab');
+		jQuery("#tab6").attr('class','tab active');
+		
+		jQuery("#t6").addClass('active');
+		jQuery("#t5").removeClass('active');
+
 }
 function submitElement5(){
+	// Change tab 
+        jQuery("#tab6").attr('class','tab');
+		jQuery("#tab7").attr('class','tab active');
+		
+		jQuery("#t7").addClass('active');
+		jQuery("#t6").removeClass('active');
 
 }
