e93ad71dc2200343b2fbefb3e29c5ed6f1dca991
karthikea <kkrishnamurthy@EA4.EDANALYTICS.local>
2015-08-21 17:15:30 -0500

    Fixed standards

--- SLO tool/standards.js
+++ SLO tool/standards.js
@@ -1,5 +1,5 @@
 var mlinks=[
-              {"name":"Math Common Core","link":"http://www.corestandards.org/Math/Content/2/introduction/"},
+              {"name":"Math Common Core","link":"http://www.corestandards.org/Math/Practice/"},
 			  {"name":"Achieve the core","link":"http://achievethecore.org/"},
 			   {"name":"Dynamic learning maps-Mathematics","link":"http://dynamiclearningmaps.org/sites/default/files/documents/Math_EEs/DLM%20Essential%20Elements%20Math%20%282013%29%20v4.pdf"},
 			   {"name":"National Board for Professional Teaching Standards","link":"http://boardcertifiedteachers.org/certificate-areas"},
@@ -10,7 +10,8 @@ var hlinks=[
               {"name":"UCLA","link":"http://www.nchs.ucla.edu/history-standards/historical-thinking-standards"},
 			  {"name":"C3 Framework for Social Studies","link":"http://www.socialstudies.org/system/files/c3/C3-Framework-for-Social-Studies.pdf"},
 			  {"name":"National Board for Professional Teaching Standards","link":"http://boardcertifiedteachers.org/certificate-areas"},
-			  {"name":"Illinois State Standards","link":"http://www.isbe.net/ils/"}
+			  {"name":"Illinois State Standards","link":"http://www.isbe.net/ils/"},
+			  {"name":"History/Social Studies Common Core","link":"http://www.corestandards.org/ELA-Literacy/RH/6-8/"}
 ];
 
 var slinks=[
@@ -21,7 +22,9 @@ var slinks=[
 
 var lalinks=[
               {"name":"Dynamic learning links","link":"http://dynamiclearningmaps.org/sites/default/files/documents/ELA_EEs/DLM%20E"},
-			   {"name":"Reading Common Core","link":"http://www.corestandards.org/Math/Content/2/introduction/"},
+			   {"name":"Reading Common Core","link":"http://www.corestandards.org/ELA-Literacy/RL/9-10/"},
+			   {"name":"Language Arts Common Core","link":"http://www.corestandards.org/ELA-Literacy/introduction/key-design-consideration/"},
+			   {"name":"Writing Common Core","link":"http://www.corestandards.org/ELA-Literacy/WHST/6-8/"},
 			   {"name":"National Board for Professional Teaching Standards","link":"http://boardcertifiedteachers.org/certificate-areas"},
 			  {"name":"Illinois State Standards","link":"http://www.isbe.net/ils/"}
 ];
@@ -36,15 +39,33 @@ var pelinks=[
               {"name":"National PE Standards, dev. by Shape ","link":"http://www.shapeamerica.org/standards/pe/index.cfm"},
 			  {"name":"National Board for Professional Teaching Standards","link":"http://boardcertifiedteachers.org/certificate-areas"},
 			  {"name":"Illinois State Standards","link":"http://www.isbe.net/ils/pdh/standards.htm#goals"}
-];
-var commonlinks=[
-	{"name":"Achieve the core","link":"http://achievethecore.org/"},
-	{"name":"Common Core","link":"http://www.corestandards.org/"},
-	{"name":"Illinois State Standards","link":"http://www.isbe.net/ils/"}
+			  
 ];
 
 
+var liblinks=[
+	{"name":"Library Standards","link":"https://www.islma.org/ISAIL.htm"}
+];
 
+var allinks=[
+	{"name":"Math Common Core","link":"http://www.corestandards.org/Math/Practice/"},
+			  {"name":"Achieve the core","link":"http://achievethecore.org/"},
+			  {"name":"Dynamic learning maps-Mathematics","link":"http://dynamiclearningmaps.org/sites/default/files/documents/Math_EEs/DLM%20Essential%20Elements%20Math%20%282013%29%20v4.pdf"},
+              {"name":"UCLA","link":"http://www.nchs.ucla.edu/history-standards/historical-thinking-standards"},
+			  {"name":"C3 Framework for Social Studies","link":"http://www.socialstudies.org/system/files/c3/C3-Framework-for-Social-Studies.pdf"},
+			  {"name":"History/Social Studies Common Core","link":"http://www.corestandards.org/ELA-Literacy/RH/6-8/"},
+			  {"name":"NGSS","link":"http://www.nextgenscience.org/next-generation-science-standards"},
+			  {"name":"Dynamic learning links","link":"http://dynamiclearningmaps.org/sites/default/files/documents/ELA_EEs/DLM%20E"},
+			   {"name":"Reading Common Core","link":"http://www.corestandards.org/ELA-Literacy/RL/9-10/"},
+			   {"name":"Language Arts Common Core","link":"http://www.corestandards.org/ELA-Literacy/introduction/key-design-consideration/"},
+			   {"name":"Writing Common Core","link":"http://www.corestandards.org/ELA-Literacy/WHST/6-8/"},
+			   {"name":"National Arts Standards","link":"http://www.nationalartsstandards.org"},
+			   {"name":"National Association for Music Education","link":"http://www.nafme.org/my-classroom/standards/"},
+			   {"name":"National PE Standards, dev. by Shape ","link":"http://www.shapeamerica.org/standards/pe/index.cfm"},
+			    {"name":"National Board for Professional Teaching Standards","link":"http://boardcertifiedteachers.org/certificate-areas"},
+			  {"name":"Illinois State Standards","link":"http://www.isbe.net/ils/pdh/standards.htm#goals"}
+];
+			   
 
 function getStandardsLinks(){
 	var subject=$('#subject').combobox('getValue');
@@ -77,6 +98,9 @@ function getStandardsLinks(){
 	if(subject=="PE"){
 		createList(pelinks);
 		}
+	if(subject=="Library"){
+		createList(liblinks);
+		}
 	/**
 	if(subject==="http://www.corestandards.org/Math/Content/2/introduction/"){
 	    va
@@ -107,6 +131,10 @@ function createList(object){
 		select.add(option);
 	}
 }
+
+$(document).ready(function(){
+	createList(allinks);
+	});
 	
 $(function(ready){
 $( ".links" ).change(function() {
